86862a9552a7442d1e76e6cb29e1163621f5b489
clive onyango <cliffonyango20@gmail.com>
2024-09-12 00:30:25 +0300

    Initial commit

---
+++ style.js
@@ -0,0 +1,127 @@
+let slideIndex = 1;
+showSlides();
+
+function showSlides(1) {
+  let slides = document.getElementsByClassName("mySlides");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.add("active");
+
+  setTimeout(showSlides, 2000); // Change the interval as needed
+}
+
+let slideIndex = 2;
+showSlides(2);
+
+function showSlides(2) {
+  let slides = document.getElementsByClassName("mySlides");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 2;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.add("active");
+
+  setTimeout(showSlides, 2000); // Change the interval as needed
+}
+let slideIndex = 3;
+showSlides();
+
+function showSlides(3) {
+  let slides = document.getElementsByClassName("mySlides");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 3;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.add("active");
+
+  setTimeout(showSlides, 2000); // Change the interval as needed
+}
+let slideIndex = 4;
+showSlides(4);
+
+function showSlides(4) {
+  let slides = document.getElementsByClassName("mySlides");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 4;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.add("active");
+
+  setTimeout(showSlides, 2000); // Change the interval as needed
+}
+let slideIndex =5;
+showSlides(5);
+
+function showSlides(5) {
+  let slides = document.getElementsByClassName("mySlides");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 5;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.add("active");
+
+  setTimeout(showSlides, 2000); // Change the interval as needed
+}
+let slideIndex = 6.jpeg;
+showSlides(6.jpeg);
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 6.jpeg;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.add("active");
+
+  setTimeout(showSlides, 2000); // Change the interval as needed
+}
